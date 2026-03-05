e6ef26d561ac334d5251f7b509c3dd106021f2aa
Adham Baskara <dammotion6@gmail.com>
2026-03-05 09:22:37 +0700

    Add Approval controller with index method for approval page

---
+++ app/Controllers/Approval.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class Approval extends BaseController
+{
+    public function index()
+    {
+        return "Halaman Approval (sementara). Login approver sudah berhasil.";
+    }
+}
