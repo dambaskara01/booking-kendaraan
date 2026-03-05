27cff6d479ab13ad4f52d1497f1796348fea4f5c
Adham Baskara <dammotion6@gmail.com>
2026-03-05 09:20:52 +0700

    Add admin dashboard and approvals routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,3 +12,6 @@ $routes->get('/login', 'Auth::login');  // menampilkan form login
 $routes->post('/login', 'Auth::loginProcess'); // memproses login
 
 $routes->get('/logout', 'Auth::logout'); // logout user
+
+$routes->get('/admin/dashboard', 'Dashboard::admin');
+$routes->get('/approvals', 'Approval::index');
