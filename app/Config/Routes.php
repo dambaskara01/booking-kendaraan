79fc17c61a45bdf2af9d52eb6b00d18c581c9f36
Adham Baskara <dammotion6@gmail.com>
2026-03-05 09:15:48 +0700

    Add authentication routes and Auth controller

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,10 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+
+$routes->get('/', 'Auth::login');       // halaman pertama langsung ke login
+
+$routes->get('/login', 'Auth::login');  // menampilkan form login
+$routes->post('/login', 'Auth::loginProcess'); // memproses login
+
+$routes->get('/logout', 'Auth::logout'); // logout user
